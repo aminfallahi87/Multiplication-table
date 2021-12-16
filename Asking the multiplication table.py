a32db3366fa70bc1ea5882d44a171669ba36152e
Amin <aminfallahi87@gmail.comgit statusgit config --global user.email aminfallahi87@gmail.com>
2021-12-16 18:21:52 +0330

    first commit

---
+++ Asking the multiplication table.py
@@ -0,0 +1,518 @@
+a = str(input('are you ready?Yes or No?'))
+
+if a == 'Yes':
+    
+    a1 = int(input("2 x 2 = "))
+    if a1 == 4:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("4")
+    a2 = int(input("2 x 3 = "))
+    if a2 == 6:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("6")
+    a3 = int(input("2 x 4 = "))
+    if a3 == 8:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("8")
+    a4 = int(input("2 x 5 = "))
+    if a4 == 10:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("10")
+    a5 = int(input("2 x 6 = "))
+    if a5 == 12:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("12")
+    a6 = int(input("2 x 7 = "))
+    if a6 == 14:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("14")
+    a7 = int(input("2 x 8 = "))
+    if a7 == 16:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("16")
+    a8 = int(input("2 x 9 = "))
+    if a8 == 18:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("18")
+    a9 = int(input("2 x 10 = "))
+    if a9 == 20:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("20")
+
+    print("__________________")
+
+    b1 = int(input("3 x 2 = "))
+    if b1 == 6:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("6")
+    b2 = int(input("3 x 3 = "))
+    if b2 == 9:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("9")
+    b3 = int(input("3 x 4 = "))
+    if b3 == 12:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("12")
+    b4 = int(input("3 x 5 = "))
+    if b4 == 15:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("15")
+    b5 = int(input("3 x 6 = "))
+    if b5 == 18:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("18")
+    b6 = int(input("3 x 7 = "))
+    if b6 == 21:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("21")
+    b7 = int(input("3 x 8 = "))
+    if b7 == 24:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("24")
+    b8 = int(input("3 x 9 = "))
+    if b8 == 27:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("27")
+    b9 = int(input("3 x 10 = "))
+    if b9 == 30:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("30")
+
+    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+
+    c1 = int(input("4 x 2 = "))
+    if c1 == 8:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("8")
+    c2 = int(input("4 x 3 = "))
+    if c2 == 12:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("12")
+    c3 = int(input("4 x 4 = "))
+    if c3 == 16:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("16")
+    c4 = int(input("4 x 5 = "))
+    if c4 == 20:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("20")
+    c5 = int(input("4 x 6 = "))
+    if c5 == 24:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("24")
+    c6 = int(input("4 x 7 = "))
+    if c6 == 28:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("28")
+    c7 = int(input("4 x 8 = "))
+    if c7 == 32:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("32")
+    c8 = int(input("4 x 9 = "))
+    if c8 == 36:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("36")
+    c9 = int(input("4 x 10 = "))
+    if c9 == 40:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("40")
+
+    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+
+    d1 = int(input("5 x 2 = "))
+    if d1 == 10:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("10")
+    d2 = int(input("5 x 3 = "))
+    if d2 == 15:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("15")
+    d3 = int(input("5 x 4 = "))
+    if d1 == 20:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("20")
+    d4 = int(input("5 x 5 = "))
+    if d4 == 25:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("25")
+    d5 = int(input("5 x 6 = "))
+    if d5 == 30:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("30")
+    d6 = int(input("5 x 7 = "))
+    if d6 == 35:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("35")
+    d7 = int(input("5 x 8 = "))
+    if d7 == 40:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("40")
+    d8 = int(input("5 x 9 = "))
+    if d8 == 45:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("45")
+    d9 = int(input("5 x 10 = "))
+    if d9 == 50:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("50")
+
+    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+
+    e1 = int(input("6 x 2 = "))
+    if e1 == 12:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("12")
+    e2 = int(input("6 x 3 = "))
+    if e2 == 18:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("18")
+    e3 = int(input("6 x 4 = "))
+    if e3 == 24:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("24")
+    e4 = int(input("6 x 5 = "))
+    if e4 == 30:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("30")
+    e5 = int(input("6 x 6 = "))
+    if e5 == 36:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("36")
+    e6 = int(input("6 x 7 = "))
+    if e6 == 42:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("42")
+    e7 = int(input("6 x 8 = "))
+    if e7 == 48:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("48")
+    e8 = int(input("6 x 9 = "))
+    if e8 == 54:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("54")
+    e9 = int(input("6 x 10 = "))
+    if e9 == 60:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("60")
+
+    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+
+    f1 = int(input("7 x 2 = "))
+    if f1 == 14:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("14")
+    f2 = int(input("7 x 3 = "))
+    if f2 == 21:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("21")
+    f3 = int(input("7 x 4 = "))
+    if f3 == 28:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("28")
+    f4 = int(input("7 x 5 = "))
+    if f4 == 35:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("35")
+    f5 = int(input("7 x 6 = "))
+    if f5 == 42:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("42")
+    f6 = int(input("7 x 7 = "))
+    if f6 == 49:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("49")
+    f7 = int(input("7 x 8 = "))
+    if f7 == 56:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("56")
+    f8 = int(input("7 x 9 = "))
+    if f8 == 63:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("63")
+    f9 = int(input("7 x 10 = "))
+    if f9 == 70:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("70")
+
+    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+
+    g1 = int(input("8 x 2 = "))
+    if g1 == 16:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("16")
+    g2 = int(input("8 x 3 = "))
+    if g2 == 24:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("24")
+    g3 = int(input("8 x 4 = "))
+    if g3 == 32:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("32")
+    g4 = int(input("8 x 5 = "))
+    if g4 == 40:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("40")
+    g5 = int(input("8 x 6 = "))
+    if g5 == 48:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("48")
+    g6 = int(input("8 x 7 = "))
+    if g6 == 56:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("56")
+    g7 = int(input("8 x 8 = "))
+    if g7 == 64:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("64")
+    g8 = int(input("8 x 9 = "))
+    if g8 == 72:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("72")
+    g9 = int(input("8 x 10 = "))
+    if g9 == 80:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("80")
+
+    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+
+    h1 = int(input("9 x 2 = "))
+    if h1 == 18:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("18")
+    h2 = int(input("9 x 3 = "))
+    if h2 == 27:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("27")
+    h3 = int(input("9 x 4 = "))
+    if h3 == 36:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("36")
+    h4 = int(input("9 x 5 = "))
+    if h4 == 45:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("45")
+    h5 = int(input("9 x 6 = "))
+    if h5 == 54:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("54")
+    h6 = int(input("9 x 7 = "))
+    if h6 == 63:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("63")
+    h7 = int(input("9 x 8 = "))
+    if h7 == 72:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("72")
+    h8 = int(input("9 x 9 = "))
+    if h8 == 81:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("81")
+    h9 = int(input("9 x 10 = "))
+    if h9 == 90:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("90")
+
+    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+
+    i1 = int(input("10 x 2 = "))
+    if i1 == 20:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("20")
+    i2 = int(input("10 x 3 = "))
+    if i2 == 30:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("30")
+    i3 = int(input("10 x 4 = "))
+    if i3 == 40:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("40")
+    i4 = int(input("10 x 5 = "))
+    if i4 == 50:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("50")
+    i5 = int(input("10 x 6 = "))
+    if i5 == 60:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("60")
+    i6 = int(input("10 x 7 = "))
+    if i6 == 70:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("70")
+    i7 = int(input("10 x 8 = "))
+    if i7 == 80:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("80")
+    i8 = int(input("10 x 9 = "))
+    if i8 == 90:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("90")
+    i9 = int(input("10 x 10 = "))
+    if i9 == 100:
+        print("✅True✅")
+    else:
+        print("❌False❌")
+        print("100")
+
+    print("❤End❤")
+else:
+    print('Okay')
